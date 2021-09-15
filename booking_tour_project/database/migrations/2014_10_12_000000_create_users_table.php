0182c688a5a1c1dd129576f1655539e217d1b5ad
VAn Lan <vanlan.ute.udn@outlook.com>
2021-09-15 22:03:03 +0700

    add migration

--- booking_tour_project/database/migrations/2014_10_12_000000_create_users_table.php
+++ booking_tour_project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('network_id')->nullable();
+            $table->integer('role_id')->unsigned();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone', 10)->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avata')->nullable();
             $table->timestamps();
         });
     }
--- booking_tour_project/routes/web.php
+++ booking_tour_project/routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin/login', function () {
+    return view('admin.auth.login');
+})->name('login');
+Route::post('/admin/login', [\App\Http\Controllers\admin\auth\LoginController::class, 'login'])->name('auth.login');
+Route::middleware(['auth'])->group(function () {
+    Route::group(['middleware' => 'checkAdmin:1'], function () {
+        Route::get('/admin/', function () {
+            return view('admin.dashboard');
+        });
+        Route::get('/admin/home', [\App\Http\Controllers\admin\HomeController::class, 'index'])->name('home');
+        Route::get('/admin/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('auth.logout');
+    });
+});
